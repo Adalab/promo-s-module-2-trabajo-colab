63a3c9c2bf7863d1226fd2289e2a0e745e3879c6
Isabel Collado Herrera <121344582+IsabelCollado@users.noreply.github.com>
2023-02-15 10:17:38 +0100

    add isa

--- src/js/0_data.js
+++ src/js/0_data.js
@@ -1,27 +1,40 @@
-'use strict';
+"use strict";
 
 const dayanaDataObject = {
-  name: 'Dayana',
-  description: 'Dayana es profesora',
-  image: 'https://adalab.es/wp-content/uploads/2022/07/dayana.jpg',
+  name: "Dayana",
+  description: "Dayana es profesora",
+  image: "https://adalab.es/wp-content/uploads/2022/07/dayana.jpg",
 };
 
 const yanelisDataObject = {
-  name: 'Yanelis',
-  description: 'Yanelis es la jefa!',
-  image: 'https://adalab.es/wp-content/uploads/Yanelis-Adalab-2022-v2.jpeg',
+  name: "Yanelis",
+  description: "Yanelis es la jefa!",
+  image: "https://adalab.es/wp-content/uploads/Yanelis-Adalab-2022-v2.jpeg",
 };
 
 const anaDataObject = {
-  name: 'Ana',
-  description: 'Ana es la profe de data',
-  image: 'https://adalab.es/wp-content/uploads/2022/07/ana.jpg',
+  name: "Ana",
+  description: "Ana es la profe de data",
+  image: "https://adalab.es/wp-content/uploads/2022/07/ana.jpg",
 };
 
 const beaDataObject = {
-  name: 'Bea',
-  description: 'Alumna promo S',
-  image: 'https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80',
+  name: "Bea",
+  description: "Alumna promo S",
+  image:
+    "https://images.unsplash.com/photo-1595433707802-6b2626ef1c91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80",
 };
 
-const data = [dayanaDataObject, yanelisDataObject, anaDataObject, beaDataObject];
+const isaDataObject = {
+  name: "Isa",
+  description: "Alumna promo S",
+  image: "",
+};
+
+const data = [
+  dayanaDataObject,
+  yanelisDataObject,
+  anaDataObject,
+  beaDataObject,
+  isaDataObject,
+];
