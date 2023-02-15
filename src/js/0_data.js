36896a839d167685bfdd59cbc27dff9d3660a7c9
Mónica Saborido <121768612+sabfiamo@users.noreply.github.com>
2023-02-15 10:10:06 +0100

    revisar cambios

--- src/js/0_data.js
+++ src/js/0_data.js
@@ -18,9 +18,9 @@ const anaDataObject = {
   image: 'https://adalab.es/wp-content/uploads/2022/07/ana.jpg',
 };
 
-const anaDataObject = {
+const moniDataObject = {
   name: 'Monica',
   description: 'Monica',
   image: '',
 };
-const data = [dayanaDataObject, yanelisDataObject, anaDataObject];
+const data = [dayanaDataObject, yanelisDataObject, anaDataObject,moniDataObject];
